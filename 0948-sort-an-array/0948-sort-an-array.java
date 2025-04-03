efbeb28ef0b31cf8db426788e8c97292b79b8e2a
Vaishnavi584 <158016152+Vaishnavi584@users.noreply.github.com>
2025-04-03 15:41:52 +0530

    Time: 29 ms (44.04%), Space: 56.2 MB (46.84%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] sortArray(int[] nums) {
+        if (nums.length <= 1) {
+            return nums;
+        }
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private void merge(int[] arr, int left, int mid, int right) {
+        int leftSize = mid - left + 1;
+        int rightSize = right - mid;
+
+        int[] leftArr = new int[leftSize];
+        int[] rightArr = new int[rightSize];
+
+        for (int i = 0; i < leftSize; i++) {
+            leftArr[i] = arr[left + i];
+        }
+        for (int j = 0; j < rightSize; j++) {
+            rightArr[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0, k = left;
+        while (i < leftSize && j < rightSize) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+            }
+        }
+
+        while (i < leftSize) {
+            arr[k++] = leftArr[i++];
+        }
+
+        while (j < rightSize) {
+            arr[k++] = rightArr[j++];
+        }
+    }
+}
