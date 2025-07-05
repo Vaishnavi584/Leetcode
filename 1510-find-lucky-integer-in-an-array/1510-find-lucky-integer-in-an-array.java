fbbc5ab182dcb44d4db92ed88707bed6afed8650
Vaishnavi584 <158016152+Vaishnavi584@users.noreply.github.com>
2025-07-05 09:47:56 +0530

    Time: 10 ms (10.78%), Space: 43 MB (97.12%) - LeetHub

---
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findLucky(int[] arr) {
+        
+        int max=-1;
+        for(int i =0; i<arr.length ; i ++){
+            int count=0;
+            int num = arr[i];
+            for(int j =0; j<arr.length; j++){
+                if(arr[j]==arr[i]){
+                    count ++; 
+                    
+               }
+                
+            }
+            if(count==arr[i]){
+                if(arr[i]>max){
+                    max=arr[i];
+                }
+            } 
+        }
+         return max;  
+    }
+}
