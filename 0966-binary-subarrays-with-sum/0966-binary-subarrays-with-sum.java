8f2a9248c58031e8cfe4d803298b36e72f41fd03
Vaishnavi584 <158016152+Vaishnavi584@users.noreply.github.com>
2025-08-26 15:50:48 +0530

    Time: 2 ms (58.75%), Space: 47 MB (99.45%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,16 +1,21 @@
 class Solution {
     public int numSubarraysWithSum(int[] arr, int goal) {
-      int count=0;
-        for(int i = 0 ;i <arr.length ;i++){
-            int sum=0;
-            for(int j=i ;j<arr.length;j++){
-                sum = sum+arr[j];
-                if(sum==goal){
-                    count++;
-                }
+         if(goal < 0) return 0; 
+     return count(arr,goal)-count(arr,goal-1);
+        
+    }
+    static int count(int []arr,int goal){
+        int sum=0;
+        int count=0;
+        int left=0;
+        for(int right=0;right<arr.length;right++){
+            sum=sum+arr[right];
+            while(left <= right && sum >goal){
+                sum-=arr[left];
+                left++;
             }
+            count=count+(right-left+1);
         }
         return count;
-        
     }
 }
