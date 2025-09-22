c211d20be11ae4f039090d917879817d6737a14a
Vaishnavi584 <158016152+Vaishnavi584@users.noreply.github.com>
2025-09-22 13:38:21 +0530

    Time: 771 ms (5.05%), Space: 47 MB (5.91%) - LeetHub

--- 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.java
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.java
@@ -1,12 +1,13 @@
 class Solution {
     public String largestOddNumber(String num) {
-        // Traverse from the end until we find an odd digit
-        for (int i = num.length() - 1; i >= 0; i--) {
-            char c = num.charAt(i);
-            if ((c - '0') % 2 == 1) { // check if odd
-                return num.substring(0, i + 1);
-            }
+      int i=0;
+      String c="";
+      while(i<num.length()){
+        if( (num.charAt(i)-'0')%2 ==1){
+            c=num.substring(0,i+1);
         }
-        return "";
+        i++;
+      }
+      return c;
     }
 }
